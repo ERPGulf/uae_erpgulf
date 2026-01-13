99b449ace8a512b5ed1b3cc197f22a347bfb612a
Aysha-sithara <asithara@htsqatar.com>
2026-01-13 11:50:52 +0300

    Update for V16

--- uae_erpgulf/uae_erpgulf/json_einvoice.py
+++ uae_erpgulf/uae_erpgulf/json_einvoice.py
@@ -57,40 +57,187 @@ def get_address(sales_invoice_doc):
         )
 
     return address_list[0]
+def get_line_extension_amount(sales_invoice_doc):
+    if not sales_invoice_doc.taxes or sales_invoice_doc.taxes[0].included_in_print_rate == 0:
+        line_extension_amount = str(round(abs(sales_invoice_doc.total), 2))
+    else:
+        if sales_invoice_doc.discount_amount:
+            line_extension_amount = str(
+                round(
+                    abs(
+                        sales_invoice_doc.base_net_total
+                        + sales_invoice_doc.get("discount_amount", 0.0)
+                    ),
+                    2,
+                )
+            )
+        else:
+            line_extension_amount = str(
+                round(abs(sales_invoice_doc.base_net_total), 2)
+            )
+    return line_extension_amount
 
+
+def get_tax_exc(sales_invoice_doc):
+    
+    if sales_invoice_doc.taxes[0].included_in_print_rate == 0:
+            tax_exclusive_amount = str(
+                round(
+                    abs(
+                        sales_invoice_doc.total
+                        - sales_invoice_doc.get("discount_amount", 0.0)
+                    ),
+                    2,
+                )
+            )
+    else:
+        if sales_invoice_doc.discount_amount ==0:
+            tax_exclusive_amount = str(
+                round(
+                    abs(
+                        sales_invoice_doc.base_net_total
+                        - sales_invoice_doc.get("discount_amount", 0.0)
+                    ),
+                    2,
+                )
+            )
+        else:
+            tax_exclusive_amount = str(
+                round(
+                    abs(
+                        sales_invoice_doc.base_net_total
+                    ),
+                    2,
+                )
+            )
+    return tax_exclusive_amount
+
+def get_tax_inclusive(sales_invoice_doc):
+    if sales_invoice_doc.taxes[0].included_in_print_rate == 0:
+            taxable_amount_1 = sales_invoice_doc.total - sales_invoice_doc.get(
+                "discount_amount", 0.0
+            )
+    else:
+        taxable_amount_1 = (
+            sales_invoice_doc.base_net_total
+            - sales_invoice_doc.get("discount_amount", 0.0)
+        )
+    tax_amount_without_retention = (
+        taxable_amount_1 * float(sales_invoice_doc.taxes[0].rate) / 100
+    )
+    if sales_invoice_doc.taxes[0].included_in_print_rate == 0:
+        tax_inclusive_amount = str(
+            round(
+                abs(
+                    sales_invoice_doc.total
+                    - sales_invoice_doc.get("discount_amount", 0.0)
+                )
+                + abs(tax_amount_without_retention),
+                2,
+            )
+        )
+    else:
+        if sales_invoice_doc.discount_amount ==0:
+
+            tax_inclusive_amount = str(
+                round(
+                    abs(
+                        sales_invoice_doc.base_net_total
+                        - sales_invoice_doc.get("discount_amount", 0.0)
+                    )
+                    + abs(tax_amount_without_retention),
+                    2,
+                )
+            )
+        else:
+            tax_inclusive_amount = str(
+                round(
+                    abs(
+                        sales_invoice_doc.base_net_total
+                    )
+                    + abs(tax_amount_without_retention),
+                    2,
+                )
+            )
+    return tax_inclusive_amount
+
+
+def get_payable_amount(sales_invoice_doc):
+    if sales_invoice_doc.taxes[0].included_in_print_rate == 0:
+            taxable_amount_1 = sales_invoice_doc.total - sales_invoice_doc.get(
+                "discount_amount", 0.0
+            )
+    else:
+        taxable_amount_1 = (
+            sales_invoice_doc.base_net_total
+            - sales_invoice_doc.get("discount_amount", 0.0)
+        )
+    tax_amount_without_retention = (
+        taxable_amount_1 * float(sales_invoice_doc.taxes[0].rate) / 100
+    )
+    if sales_invoice_doc.taxes[0].included_in_print_rate == 0:
+        total_amount = round(
+            abs(
+                sales_invoice_doc.total
+                - sales_invoice_doc.get("discount_amount", 0.0)
+            )
+            + abs(tax_amount_without_retention),
+            2,
+        )
+    else:
+        if sales_invoice_doc.discount_amount ==0:
+            total_amount = round(
+                abs(
+                    sales_invoice_doc.base_net_total
+                    - sales_invoice_doc.get("discount_amount", 0.0)
+                )
+                + abs(tax_amount_without_retention),
+                2,
+            )
+        else:
+            total_amount=round(
+                abs(
+                    sales_invoice_doc.base_net_total
+                )
+                + abs(tax_amount_without_retention),
+                2,
+            )
+    return total_amount
 
 def build_uae_invoice_json(invoice_number):
     sales_invoice_doc = frappe.get_doc("Sales Invoice", invoice_number)
     company_doc = frappe.get_doc("Company", sales_invoice_doc.company)
     customer_doc = frappe.get_doc("Customer", sales_invoice_doc.customer)
     address = get_address(sales_invoice_doc)
-    
-    country_dict = country_code_mapping()
     address_data = None
 
-    # if not address_data:
-    #     frappe.throw(_("Customer address not found"))
+    if sales_invoice_doc.customer_address:
+        address_data = frappe.get_doc("Address", sales_invoice_doc.customer_address)
+    elif customer_doc.customer_primary_address:
+        address_data = frappe.get_doc("Address", customer_doc.customer_primary_address)
+
+    if not address_data:
+        frappe.throw(_("Customer address not found"))
+
+    # ---------------- COUNTRY CODE ----------------
+    country_dict = country_code_mapping()
 
     if address_data.country and address_data.country.lower() in country_dict:
-
         country_code1 = country_dict[address_data.country.lower()]
-
     else:
-        country_code1 = "United Arabian Emirates"   
-    if int(frappe.__version__.split(".", maxsplit=1)[0]) == 13:
-            if sales_invoice_doc.customer_address:
-                address_data = frappe.get_doc(
-                    "Address", sales_invoice_doc.customer_address
-                )
-            else:
-                if customer_doc.customer_primary_address:
-                    address_data = frappe.get_doc(
-                        "Address", customer_doc.customer_primary_address
-                    )
-    if address_data.country and address_data.country.lower() in country_dict:
-                    country_code1 = country_dict[
-                        address.country.lower()
-                    ]
+        country_code1 = "AE" 
+    line_extension_amount = get_line_extension_amount(sales_invoice_doc)  
+    tax_exclusive_amount = get_tax_exc(sales_invoice_doc)
+    tax_inclusive_amount = get_tax_inclusive(sales_invoice_doc)
+    payable_amount = get_payable_amount(sales_invoice_doc)
+ 
+
+    # Assuming payable_amount and tax_inclusive_amount are strings
+    payable_amount_float = float(payable_amount)
+    tax_inclusive_amount_float = float(tax_inclusive_amount)
+
+    payable_rounding_amount = (payable_amount_float - tax_inclusive_amount_float)
+
     invoice = {
         "invoice_id": sales_invoice_doc.name,
         "issue_date": str(sales_invoice_doc.posting_date),
@@ -100,7 +247,7 @@ def build_uae_invoice_json(invoice_number):
         "document_currency_code": sales_invoice_doc.currency,
         "note": "Tax Invoice",
         "tax_point_date": str(sales_invoice_doc.due_date) if sales_invoice_doc.due_date else None,
-        "accounting_cost": "4025:123:4343",
+        "accounting_cost": "4025:123:4343", #sales_invoice_doc.cost_centre
         "buyer_reference": get_icv_code(invoice_number),
 
         "invoice_period": {
@@ -116,7 +263,7 @@ def build_uae_invoice_json(invoice_number):
 
         "billing_reference": {
             "invoice_document_reference": {
-                "id": get_icv_code(invoice_number),
+                "id": str(invoice_number),
                 "issue_date": str(sales_invoice_doc.posting_date)
             }
         },
@@ -135,7 +282,7 @@ def build_uae_invoice_json(invoice_number):
                     "postal_zone": address.get("pincode"),
                     "country_subentity":  address.get("state"),
                     "address_line": address.get("address_line2"),
-                    "country": {"identification_code": country_code}
+                    "country": {"identification_code": country_code1}
                 },
                 "party_tax_scheme": {
                     "company_id": company_doc.tax_id,
@@ -178,7 +325,7 @@ def build_uae_invoice_json(invoice_number):
                     "tax_scheme": {"id": "VAT"}
                 },
                 "party_legal_entity": {
-                    "registration_name": "Buyer Legal Name",
+                    "registration_name": customer_doc.customer_name,
                     "company_id": {
                         "value":customer_doc.tax_id ,
                         "scheme_agency_id": "TL",
@@ -190,15 +337,15 @@ def build_uae_invoice_json(invoice_number):
             }
         },
         "legal_monetary_total": {
-                "line_extension_amount": 18307.50,
-                "tax_exclusive_amount": 15661.50,
-                "tax_inclusive_amount": 15777.20,
-                "allowance_total_amount": 2725.00,
-                "charge_total_amount": 79.00,
-                "prepaid_amount": 10000.00,
-                "payable_rounding_amount": 0.30,
-                "payable_amount": 5777.50,
-                "currency_id": "AED"
+                "line_extension_amount": line_extension_amount,
+                "tax_exclusive_amount": tax_exclusive_amount,
+                "tax_inclusive_amount": tax_inclusive_amount,
+                "allowance_total_amount":  str(abs(sales_invoice_doc.get("discount_amount", 0.0))),
+                "charge_total_amount": str(abs(sales_invoice_doc.get("base_change_amount", 0.0))),
+                "prepaid_amount": 0,
+                "payable_rounding_amount": payable_rounding_amount,
+                "payable_amount": payable_amount,
+                "currency_id": sales_invoice_doc.currency
             },
             
 
@@ -223,15 +370,18 @@ def build_uae_invoice_json(invoice_number):
         tax_rate = list(tax_dict.values())[0]
         invoice["invoice_line"].append({
             "id": str(idx),
+            "note": "Please check the invoice",
             "invoiced_quantity": str(item.qty),
-            "unit_code": item.uom,
+            "invoiced_quantity_unit_code": item.uom,
+            "currency_id": sales_invoice_doc.price_list_currency,
+            "accounting_cost": item.cost_center,
             "line_extension_amount": r2(net),
             "item": {
                 "name": item.item_name,
                 "description": item.description,
                 "commodity_code": item.item_code,
                 "classification": [{
-                    "code": "hs001",
+                    "code": item.item_code,
                     "scheme": "HS"
                 }] ,
                 # if item.custom_hs_code else [],
@@ -241,7 +391,9 @@ def build_uae_invoice_json(invoice_number):
             },
             "price": {
                 "price_amount": r2(item.rate),
-                "base_quantity": "1"
+                "currency_id":sales_invoice_doc.currency,
+                "base_quantity": "1",
+                "base_quantity_unit_code": item.uom
             }
         })
 
@@ -254,36 +406,52 @@ def build_uae_invoice_json(invoice_number):
     }
 
     return invoice
-
 def save_and_attach_invoice_json(invoice_number):
     """
-    Builds UAE invoice JSON, saves it as a file, and attaches it to the Sales Invoice
+    Builds UAE invoice JSON, saves it as a file, and attaches it to the Sales Invoice.
+    If the file already exists, it will be overwritten.
     """
 
     invoice_json = build_uae_invoice_json(invoice_number)
-
     json_content = json.dumps(invoice_json, indent=4, ensure_ascii=False)
 
     file_name = f"{invoice_number}_uae_invoice.json"
 
-    file_doc = frappe.get_doc({
-        "doctype": "File",
-        "file_name": file_name,
-        "is_private": 1,
-        "content": json_content,
-        "attached_to_doctype": "Sales Invoice",
-        "attached_to_name": invoice_number
-    })
+    # 🔎 Check if file already exists
+    existing_file = frappe.get_all(
+        "File",
+        filters={
+            "attached_to_doctype": "Sales Invoice",
+            "attached_to_name": invoice_number,
+            "file_name": file_name,
+        },
+        limit=1,
+    )
 
-    file_doc.insert(ignore_permissions=True)
+    if existing_file:
+        # 🔁 Overwrite existing file
+        file_doc = frappe.get_doc("File", existing_file[0].name)
+        file_doc.content = json_content
+        file_doc.save(ignore_permissions=True)
+
+    else:
+        # ➕ Create new file
+        file_doc = frappe.get_doc({
+            "doctype": "File",
+            "file_name": file_name,
+            "is_private": 1,
+            "content": json_content,
+            "attached_to_doctype": "Sales Invoice",
+            "attached_to_name": invoice_number,
+        })
+        file_doc.insert(ignore_permissions=True)
+
     frappe.db.commit()
 
-    # ✅ RETURN DATA
     return {
         "file_name": file_doc.file_name,
-        "file_url": file_doc.file_url
+        "file_url": file_doc.file_url,
     }
-
 
 @frappe.whitelist()
 def send_invoice_json(invoice_number):
